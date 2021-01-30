f1e578e6cd8a3b83eaaa35b026d32b1bc17dca96
Ivanyany <754339948@qq.com>
2021-01-30 12:31:39 +0800

    完成首页文章列表

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <div id="app">
-    <h1>Ivan头条</h1>
-    <!-- 路由的出口 -->
-    <router-view/>
+    <!-- 一级路由的出口，路由表中所有的顶层路由都是一级路由
+      因为 router-view 是一级路由出口
+      keep-alive 仅对该路由出口渲染的组件有效
+    -->
+    <keep-alive :include="cachePages">
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
-  name: 'App'
+  name: 'App',
+  computed: {
+    ...mapState(['cachePages'])
+  }
 }
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+.box {
+  width: 200px;
+  height: 100px;
+  background-color: pink;
+}
+</style>
